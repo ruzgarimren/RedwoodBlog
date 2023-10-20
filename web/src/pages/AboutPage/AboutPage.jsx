ee0be9adf812c69dd21e929510206bd4948f33c6
Ruzgar Imren <135638786+ruzgarimren@users.noreply.github.com>
2023-10-20 19:02:30 +0300

    Initial commit

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,24 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <header>
+        <h1>About Page</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default AboutPage
